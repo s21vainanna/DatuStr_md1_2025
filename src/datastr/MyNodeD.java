5f4c73b647f3d9f8bedd2b3da82aea6e1d0c4c6a
anna <s21vainanna@venta.lv>
2025-04-11 21:20:55 +0300

    added .gitignore and made some changes o MyNodeD<T>

--- src/datastr/MyNodeD.java
+++ src/datastr/MyNodeD.java
@@ -4,10 +4,12 @@ public class MyNodeD<T> {
 	
 	private T element;
 	private MyNodeD<T> next = null;
+	private MyNodeD<T> prev = null;
 	
 	public MyNodeD(T element) {
 		this.element = element;
 		this.next = null;
+		this.prev = null;
 	}
 	
 	public T getElement() {
@@ -28,6 +30,14 @@ public class MyNodeD<T> {
 		this.next = next;
 	}
 	
+	public MyNodeD<T> getPrev() {
+		return prev;
+	}
+
+	public void setPrev(MyNodeD<T> prev) {
+		this.prev = prev;
+	}
+
 	@Override
 	public String toString() {
 		return element + "";
